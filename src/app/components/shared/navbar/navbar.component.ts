8509c53b1d44a9601b0dd0f191f4603d647e6f5c
Daniel Díaz <dalejodc@gmail.com>
2019-03-30 17:59:28 -0600

    Sticky desktop navbar

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
 })
+
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+    sticky: boolean = false;
+    showMobileMenu: boolean = false;
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    @HostListener('window:scroll', ['$event'])
+    onWindowScroll($event) {
+        let navbar = document.getElementById("navbar");
+
+        if (window.scrollY > 73) {
+            navbar.classList.add("sticky", "animated", "slideInDown");
+        } else {
+            navbar.classList.remove("sticky", "animated", "slideInDown");
+        }
+
+    }
 
 }
