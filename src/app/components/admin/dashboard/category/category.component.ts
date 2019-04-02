87c22fc88b2634871289300c11aec54a6e09deb1
Daniel Díaz <dalejodc@gmail.com>
2019-04-01 19:08:32 -0600

    Firs step validation

--- src/app/components/admin/dashboard/category/category.component.ts
+++ src/app/components/admin/dashboard/category/category.component.ts
@@ -16,6 +16,8 @@ export class CategoryComponent implements OnInit {
     categories: Category[] = [];
     category: Category = new Category();
 
+    errorMessage: boolean = false;
+
     constructor(
         private _categoryService: CategoryService,
         private _route: Router
@@ -34,7 +36,22 @@ export class CategoryComponent implements OnInit {
     }
 
     save() {
-        // console.log(this.category);
+        console.log(this.category)
+
+        if (Object.keys(this.category).length === 0) {
+            
+            const Toast = swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000
+              });
+              
+              Toast({
+                type: 'error',
+                title: 'All fields are required'
+              })
+        }
 
         this._categoryService.saveCategory(this.category).subscribe(response => {
             console.log(response);
@@ -51,24 +68,24 @@ export class CategoryComponent implements OnInit {
             })
 
             this.category = new Category();
-            
+
             this.getCategories();
         })
     }
-    
-    edit(id){
+
+    edit(id) {
         console.log(id);
-        
-        this._categoryService.getCategoryById(id).subscribe(response=>{
+
+        this._categoryService.getCategoryById(id).subscribe(response => {
             console.log(response);
             this.category = response;
-            
+
             this.openModal();
-        
+
         })
     }
 
-    cancel(){
+    cancel() {
         this.category = new Category();
     }
 
